058ff17f6cb3cba13251e6c569b42bc91ba38b32
Ismail Badrezzamane <drismailbadrez@gmail.com>
2022-09-15 12:48:36 +0100

    Last modifications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def sendmail
-    ContactMailer.with(contact: contact_params).new_contact_email.deliver_later
+    # ContactMailer.with(contact: contact_params).new_contact_email.deliver_later
+    redirect_to profile_path
   end
 
   def contact_params
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
       :port => 587,
       :domain => 'localhost:3000',
       :user_name => "ev212.contact@gmail.com",
-      :password => "amineismaildriss",
+      :password => "XXXXXXXX",
       :authentication => :plain,
       :enable_starttls_auto => true
   }
