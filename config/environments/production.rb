352b36ef68e5d30ff536351804893bb55e01e5c0
Ismail Badrezzamane <drismailbadrez@gmail.com>
2022-09-15 11:48:20 +0100

    Modified Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
-      :port => 25,
+      :port => 587,
       :domain => 'localhost:3000',
       :user_name => "ev212.contact@gmail.com",
       :password => "amineismaildriss",
